864e141d4eaf928ef9a15c0603369de5c8255d68
Samet <samederturk555@gmail.com>
2024-01-29 21:12:08 +0300

    mongoDB atlas and mongoose config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,26 @@
+import "dotenv/config";
+import env from './util/validateEnv'
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
-const port = 5000;
 
 app.get("/", (req, res) => {
-    res.send('Fenerin maçi var!');
+    res.send('Hello World!');
 });
 
-//bunu neden göremiyorum?
-app.listen(port, () => {
-    console.log("Server running on port: "  + port);
-});
+const port = env.PORT;
+
+// require('dotenv').config();
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+    .then(() => {
+        console.log("Mongoose connected");
+
+        //bunu neden göremiyorum?
+        //bunu mongoose içerisine eklememizin nedeni? Eğer connect sağlanamazsa sunucuyu başlatılmasın.
+        app.listen(port, () => {
+            console.log("Server running on port: "  + port);
+        });
+    })
+    .catch(console.error)
+
+
