2bee0b656a3bd8c0f30c51005b0daf19ccc61d2b
Samet <samederturk555@gmail.com>
2024-02-03 01:00:05 +0300

    update notes added

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -11,4 +11,6 @@ router.get("/:noteId", NotesController.getNote);
 //aynı route olmasında sakınca yoktur. Çünkü http request'leri farklı. get and post
 router.post("/", NotesController.createNotes);
 
+router.patch("/:noteId" , NotesController.updateNote);
+
 export default router;
